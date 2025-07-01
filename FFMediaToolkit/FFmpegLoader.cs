db71a9d40be43e8516fc1fb8ae8b849d1b4216a0
MaKrotos <Darklider33@gmail.com>
2025-07-01 11:42:49 +0300

    Fix FFMPEG conflict

--- FFMediaToolkit/FFmpegLoader.cs
+++ FFMediaToolkit/FFmpegLoader.cs
@@ -1,10 +1,11 @@
 ﻿namespace FFMediaToolkit
 {
+    using FFMediaToolkit.Interop;
+    using FFmpeg.AutoGen;
     using System;
     using System.IO;
     using System.Runtime.InteropServices;
-    using FFMediaToolkit.Interop;
-    using FFmpeg.AutoGen;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// Contains methods for managing FFmpeg libraries.
@@ -173,6 +174,149 @@
         internal static void HandleLibraryLoadError(Exception exception)
         {
             throw new DllNotFoundException($"Cannot load FFmpeg libraries from {FFmpegPath} directory.\nRequired FFmpeg version: 6.x (shared build)\nMake sure the \"Build\"Prefer 32-bit\" option in the project settings is turned off.\nFor more information please see https://github.com/radek-k/FFMediaToolkit#setup", exception);
+        }
+
+
+        public static unsafe Task DownloadAndConvertWithFFmpegAutogen(string inputUrl, string outputPath)
+        {
+            return Task.Run(() =>
+            {
+                // Регистрируем все кодеки и форматы
+                ffmpeg.avformat_network_init();
+
+                AVFormatContext* pFormatContext = null;
+                AVFormatContext* pOutputFormatContext = null;
+
+                try
+                {
+                    // Открываем входной поток
+                    if (ffmpeg.avformat_open_input(&pFormatContext, inputUrl, null, null) != 0)
+                    {
+                        throw new Exception("Could not open input file.");
+                    }
+
+                    // Получаем информацию о потоке
+                    if (ffmpeg.avformat_find_stream_info(pFormatContext, null) < 0)
+                    {
+                        throw new Exception("Could not find stream information.");
+                    }
+
+                    // Находим аудио поток
+                    int audioStreamIndex = -1;
+                    for (int i = 0; i < pFormatContext->nb_streams; i++)
+                    {
+                        if (pFormatContext->streams[i]->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_AUDIO)
+                        {
+                            audioStreamIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (audioStreamIndex == -1)
+                    {
+                        throw new Exception("Could not find audio stream.");
+                    }
+
+                    // Получаем параметры кодека
+                    AVCodecParameters* pCodecParameters = pFormatContext->streams[audioStreamIndex]->codecpar;
+                    AVCodec* pCodec = ffmpeg.avcodec_find_decoder(pCodecParameters->codec_id);
+
+                    if (pCodec == null)
+                    {
+                        throw new Exception("Unsupported codec.");
+                    }
+
+                    // Создаем контекст кодека
+                    AVCodecContext* pCodecContext = ffmpeg.avcodec_alloc_context3(pCodec);
+                    if (ffmpeg.avcodec_parameters_to_context(pCodecContext, pCodecParameters) < 0)
+                    {
+                        throw new Exception("Could not copy codec parameters to context.");
+                    }
+
+                    // Открываем кодек
+                    if (ffmpeg.avcodec_open2(pCodecContext, pCodec, null) < 0)
+                    {
+                        throw new Exception("Could not open codec.");
+                    }
+
+                    // Создаем контекст выходного файла
+                    if (ffmpeg.avformat_alloc_output_context2(&pOutputFormatContext, null, null, outputPath) < 0)
+                    {
+                        throw new Exception("Could not create output context.");
+                    }
+
+                    // Создаем выходной поток
+                    AVStream* pOutputStream = ffmpeg.avformat_new_stream(pOutputFormatContext, null);
+                    if (pOutputStream == null)
+                    {
+                        throw new Exception("Could not create output stream.");
+                    }
+
+                    // Устанавливаем параметры кодека для выходного потока
+                    if (ffmpeg.avcodec_parameters_copy(pOutputStream->codecpar, pCodecParameters) < 0)
+                    {
+                        throw new Exception("Could not copy codec parameters.");
+                    }
+
+                    // Открываем выходной файл
+                    if ((ffmpeg.avio_open(&pOutputFormatContext->pb, outputPath, ffmpeg.AVIO_FLAG_WRITE) < 0))
+                    {
+                        throw new Exception("Could not open output file.");
+                    }
+
+                    // Записываем заголовок файла
+                    if (ffmpeg.avformat_write_header(pOutputFormatContext, null) < 0)
+                    {
+                        throw new Exception("Could not write header.");
+                    }
+
+                    // Читаем и записываем пакеты
+                    AVPacket* pPacket = ffmpeg.av_packet_alloc();
+                    AVFrame* pFrame = ffmpeg.av_frame_alloc();
+
+                    while (ffmpeg.av_read_frame(pFormatContext, pPacket) >= 0)
+                    {
+                        if (pPacket->stream_index == audioStreamIndex)
+                        {
+                            // Перемещаем пакет в выходной поток
+                            pPacket->stream_index = pOutputStream->index;
+
+                            // Пересчитываем временные метки
+                            ffmpeg.av_packet_rescale_ts(pPacket,
+                                pFormatContext->streams[audioStreamIndex]->time_base,
+                                pOutputStream->time_base);
+
+                            // Записываем пакет
+                            if (ffmpeg.av_interleaved_write_frame(pOutputFormatContext, pPacket) < 0)
+                            {
+                                throw new Exception("Error while writing packet.");
+                            }
+                        }
+
+                        ffmpeg.av_packet_unref(pPacket);
+                    }
+
+                    // Записываем завершающую часть файла
+                    ffmpeg.av_write_trailer(pOutputFormatContext);
+                }
+                finally
+                {
+                    // Освобождаем ресурсы
+                    if (pFormatContext != null)
+                    {
+                        ffmpeg.avformat_close_input(&pFormatContext);
+                    }
+                    if (pOutputFormatContext != null && pOutputFormatContext->pb != null)
+                    {
+                        ffmpeg.avio_closep(&pOutputFormatContext->pb);
+                    }
+                    if (pOutputFormatContext != null)
+                    {
+                        ffmpeg.avformat_free_context(pOutputFormatContext);
+                    }
+                    ffmpeg.avformat_network_deinit();
+                }
+            });
         }
     }
 }
--- VK UI3/DownloadTrack/PlayListDownload.cs
+++ VK UI3/DownloadTrack/PlayListDownload.cs
@@ -169,7 +169,7 @@ namespace VK_UI3.DownloadTrack
                         if (!Directory.Exists(location)) Directory.CreateDirectory(location);
 
                         // Используем FFmpeg.Autogen для загрузки и конвертации
-                        await DownloadAndConvertWithFFmpegAutogen(input, outputTemp);
+                        await FFMediaToolkit.FFmpegLoader.DownloadAndConvertWithFFmpegAutogen(input, outputTemp);
 
                         MakeTags(outputTemp, a);
                         File.Move(outputTemp, output);
@@ -210,147 +210,7 @@ namespace VK_UI3.DownloadTrack
             }, cts.Token);
         }
 
-        private unsafe Task DownloadAndConvertWithFFmpegAutogen(string inputUrl, string outputPath)
-        {
-            return Task.Run(() =>
-            {
-                // Регистрируем все кодеки и форматы
-                ffmpeg.avformat_network_init();
-
-                AVFormatContext* pFormatContext = null;
-                AVFormatContext* pOutputFormatContext = null;
-
-                try
-                {
-                    // Открываем входной поток
-                    if (ffmpeg.avformat_open_input(&pFormatContext, inputUrl, null, null) != 0)
-                    {
-                        throw new Exception("Could not open input file.");
-                    }
-
-                    // Получаем информацию о потоке
-                    if (ffmpeg.avformat_find_stream_info(pFormatContext, null) < 0)
-                    {
-                        throw new Exception("Could not find stream information.");
-                    }
-
-                    // Находим аудио поток
-                    int audioStreamIndex = -1;
-                    for (int i = 0; i < pFormatContext->nb_streams; i++)
-                    {
-                        if (pFormatContext->streams[i]->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_AUDIO)
-                        {
-                            audioStreamIndex = i;
-                            break;
-                        }
-                    }
-
-                    if (audioStreamIndex == -1)
-                    {
-                        throw new Exception("Could not find audio stream.");
-                    }
-
-                    // Получаем параметры кодека
-                    AVCodecParameters* pCodecParameters = pFormatContext->streams[audioStreamIndex]->codecpar;
-                    AVCodec* pCodec = ffmpeg.avcodec_find_decoder(pCodecParameters->codec_id);
-
-                    if (pCodec == null)
-                    {
-                        throw new Exception("Unsupported codec.");
-                    }
-
-                    // Создаем контекст кодека
-                    AVCodecContext* pCodecContext = ffmpeg.avcodec_alloc_context3(pCodec);
-                    if (ffmpeg.avcodec_parameters_to_context(pCodecContext, pCodecParameters) < 0)
-                    {
-                        throw new Exception("Could not copy codec parameters to context.");
-                    }
-
-                    // Открываем кодек
-                    if (ffmpeg.avcodec_open2(pCodecContext, pCodec, null) < 0)
-                    {
-                        throw new Exception("Could not open codec.");
-                    }
-
-                    // Создаем контекст выходного файла
-                    if (ffmpeg.avformat_alloc_output_context2(&pOutputFormatContext, null, null, outputPath) < 0)
-                    {
-                        throw new Exception("Could not create output context.");
-                    }
-
-                    // Создаем выходной поток
-                    AVStream* pOutputStream = ffmpeg.avformat_new_stream(pOutputFormatContext, null);
-                    if (pOutputStream == null)
-                    {
-                        throw new Exception("Could not create output stream.");
-                    }
-
-                    // Устанавливаем параметры кодека для выходного потока
-                    if (ffmpeg.avcodec_parameters_copy(pOutputStream->codecpar, pCodecParameters) < 0)
-                    {
-                        throw new Exception("Could not copy codec parameters.");
-                    }
-
-                    // Открываем выходной файл
-                    if ((ffmpeg.avio_open(&pOutputFormatContext->pb, outputPath, ffmpeg.AVIO_FLAG_WRITE) < 0))
-                    {
-                        throw new Exception("Could not open output file.");
-                    }
-
-                    // Записываем заголовок файла
-                    if (ffmpeg.avformat_write_header(pOutputFormatContext, null) < 0)
-                    {
-                        throw new Exception("Could not write header.");
-                    }
-
-                    // Читаем и записываем пакеты
-                    AVPacket* pPacket = ffmpeg.av_packet_alloc();
-                    AVFrame* pFrame = ffmpeg.av_frame_alloc();
-
-                    while (ffmpeg.av_read_frame(pFormatContext, pPacket) >= 0)
-                    {
-                        if (pPacket->stream_index == audioStreamIndex)
-                        {
-                            // Перемещаем пакет в выходной поток
-                            pPacket->stream_index = pOutputStream->index;
-
-                            // Пересчитываем временные метки
-                            ffmpeg.av_packet_rescale_ts(pPacket,
-                                pFormatContext->streams[audioStreamIndex]->time_base,
-                                pOutputStream->time_base);
-
-                            // Записываем пакет
-                            if (ffmpeg.av_interleaved_write_frame(pOutputFormatContext, pPacket) < 0)
-                            {
-                                throw new Exception("Error while writing packet.");
-                            }
-                        }
-
-                        ffmpeg.av_packet_unref(pPacket);
-                    }
-
-                    // Записываем завершающую часть файла
-                    ffmpeg.av_write_trailer(pOutputFormatContext);
-                }
-                finally
-                {
-                    // Освобождаем ресурсы
-                    if (pFormatContext != null)
-                    {
-                        ffmpeg.avformat_close_input(&pFormatContext);
-                    }
-                    if (pOutputFormatContext != null && pOutputFormatContext->pb != null)
-                    {
-                        ffmpeg.avio_closep(&pOutputFormatContext->pb);
-                    }
-                    if (pOutputFormatContext != null)
-                    {
-                        ffmpeg.avformat_free_context(pOutputFormatContext);
-                    }
-                    ffmpeg.avformat_network_deinit();
-                }
-            });
-        }
+        
 
         public static void ResumeOnlyFirst()
         {
